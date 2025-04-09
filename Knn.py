95dfd07da6591db38bc9ec0dc87713f6b52e937d
sunisa404 <sunisa11398@gmail.com>
2025-04-09 12:04:10 +0700

    .commit

--- Knn.py
+++ Knn.py
@@ -21,3 +21,39 @@ with col2:
 with col3:
    st.header("Setosa")
    st.image("./img/iris3.jpg")
+
+html_7 = """
+<div style="background-color:#c5f18a;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>ข้อมูล iris หรือข้อมูลดอกไม้สำหรับทำนาย</h5></center>
+</div>
+"""
+st.markdown(html_7, unsafe_allow_html=True)
+st.markdown("")
+st.markdown("")
+
+st.subheader("ข้อมูลส่วนแรก 10 แถว")
+dt = pd.read_csv("./data/iris-3.csv")
+st.write(dt.head(10))
+st.subheader("ข้อมูลส่วนสุดท้าย 10 แถว")
+st.write(dt.tail(10))
+
+dt1 = dt['petallength'].sum()
+dt2 = dt['petalwidth'].sum()
+dt3 = dt['sepallength'].sum()
+dt4 = dt['sepalwidth'].sum()
+
+dx = [dt1, dt2, dt3, dt4]
+dx2 = pd.DataFrame(dx, index=["d1", "d2", "d3", "d4"])
+
+if st.button("แสดงการจินตทัศน์ข้อมูล"):
+   #st.write(dt.head(10))
+   st.bar_chart(dx2)
+   st.button("ไม่แสดงข้อมูล")
+else:
+    st.write("ไม่แสดงข้อมูล")
+
+html_8 = """
+<div style="background-color:#6BD5DA;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>ทำนายข้อมูล</h5></center>
+</div>
+"""
