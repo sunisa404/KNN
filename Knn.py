b0d6f40fb6cb85c41efa41e99ba36338c2b8316e
sunisa404 <sunisa11398@gmail.com>
2025-04-09 14:02:52 +0700

    .commit

--- Knn.py
+++ Knn.py
@@ -74,7 +74,7 @@ st.write(dt.describe())
 
 # การเลือกแสดงกราฟตามฟีเจอร์
 st.subheader("📌 เลือกฟีเจอร์เพื่อดูการกระจายข้อมูล")
-feature = st.selectbox("เลือกฟีเจอร์", dt.columns[:-1])
+feature = st.selectbox("เลือกฟีเจอร์", dt.columns[:-1])# เอาทุกคอมลัมม์แต่ไม่เอาคอมลัมม์สุดท้าย(Variety)
 
 # วาดกราฟ boxplot
 st.write(f"### 🎯 Boxplot: {feature} แยกตามชนิดของดอกไม้")
@@ -83,7 +83,44 @@ sns.boxplot(data=dt, x='variety', y=feature, ax=ax)
 st.pyplot(fig)
 
 # วาด pairplot
-if st.checkbox("แสดง Pairplot (ใช้เวลาประมวลผลเล็กน้อย)"):
+if st.checkbox("แสดง Pairplot (ใช้เวลาประมวลผลเล็กน้อย)"):# ถ้ามีการเช็คตัวนี้ ให้ทำการสั่งกระจายข้อมูล
     st.write("### 🌺 Pairplot: การกระจายของข้อมูลทั้งหมด")
     fig2 = sns.pairplot(dt, hue='variety')
-    st.pyplot(fig2)
+    st.pyplot(fig2)# ให้ plot ข้อมูลออกมา
+
+html_8 = """
+<div style="background-color:#6BD5DA;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>ทำนายข้อมูล</h5></center>
+</div>
+"""
+st.markdown(html_8, unsafe_allow_html=True)
+st.markdown("")
+
+pt_len = st.slider("กรุณาเลือกข้อมูล petal.length")
+pt_wd = st.slider("กรุณาเลือกข้อมูล petal.width")
+
+sp_len = st.number_input("กรุณาเลือกข้อมูล sepal.length")
+sp_wd = st.number_input("กรุณาเลือกข้อมูล sepal.width")
+
+if st.button("ทำนายผล"):
+    #st.write("ทำนาย")
+   dt = pd.read_csv("./data/iris-3.csv") 
+   X = dt.drop('variety', axis=1)
+   y = dt.variety   
+
+   Knn_model = KNeighborsClassifier(n_neighbors=3)
+   Knn_model.fit(X, y)# เข้าข้อมูลมาทำการ Train
+    
+   x_input = np.array([[pt_len, pt_wd, sp_len, sp_wd]])
+   st.write(Knn_model.predict(x_input))
+   
+   out=Knn_model.predict(x_input)
+
+   if out[0] == 'Setosa':
+    st.image("./img/iris1.jpg")
+   elif out[0] == 'Versicolor':       
+    st.image("./img/iris2.jpg")
+   else:
+    st.image("./img/iris3.jpg")
+else:
+    st.write("ไม่ทำนาย")
